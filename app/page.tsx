a0b14351f8a6e5968535bf3478227d37b38e7e39
eruvalcaba94 <eric.s.ruvalcaba@gmail.com>
2023-08-13 18:41:20 -0700

    project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <header className='grid grid-cols-4 border-4 border-black border-solid rounded-lg mx-2 mt-2'>
+        <div className='col-span-3'>
+          <p className='text-center p-3'>Header</p>
+        </div>
+        <nav className='border-4 border-red-300 border-solid rounded-lg mx-2'>
+          <p className='text-center p-3'>Nav Bar</p>
+        </nav>
+      </header>
+      <div className='grid grid-cols-6'>
+        <aside className='border-4 border-black border-solid rounded-lg mx-2 mt-2'>
+          <p className='text-center p-3'>Aside</p>
+        </aside>
+        <main className='col-span-5 border-4 border-black border-solid rounded-lg mx-2 mt-2'>
+          <p className='text-center p-3'>Body</p>
+        </main>
+      </div>
+      <footer className='border-4 border-black border-solid rounded-lg mx-2 mt-2'>
+        <p className='text-center p-3'>Footer</p>
+      </footer>
+    </div>
+  );
 }
